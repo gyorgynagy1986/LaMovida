39612afba27b622b544e3551e856179ff6c29c94
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-03-16 21:10:34 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// STICKY NAVIGATIOn
+
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const prefixHeight = document.querySelector('.nav_container')
+const navLogo = document.querySelector('.nav-logo');
+const navLogoSticky = document.querySelector('.nav-logo-sticky');
+
+
+console.log(navLogo);
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) {
+    nav.classList.add('sticky');
+    nav.classList.add('slide-in-top');
+    prefixHeight.classList.add('nav-c-10');
+    navLogo.classList.add('display-none');
+    navLogoSticky.classList.remove('display-none');
+  } else {
+    nav.classList.remove('sticky');
+    nav.classList.remove('slide-in-top');
+    prefixHeight.classList.remove('nav-c-10');
+    navLogo.classList.remove('display-none');
+    navLogoSticky.classList.add('display-none');
+
+  }
+}
+
+const headerObserver = new IntersectionObserver(
+  stickyNav, {
+    root:  null,
+    threshold: 0,
+    rootMargin : '-90px'
+  }
+);
+
+headerObserver.observe(header);
+
+
+// STICKY NAVIGATION END
+
+
+const svgCrab = document.querySelector('.svg-container-crab');
+const svgFish = document.querySelector('.svg-container-fish');
+
+console.log(svgCrab);
+
+
+
+svgCrab.style.bottom = `${window.pageYOffset + 450}px`
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(scrollTop);
+    
+    svgCrab.style.bottom = `${scrollTop * 2 / 22 + 450}px`
+    svgFish.style.bottom = `${scrollTop * 3 / 22}px`
+  });
+
+
